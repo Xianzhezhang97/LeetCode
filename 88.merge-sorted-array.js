3a59377d9832b6544b0e5095f307073c015e6e58
Xianzhezhang97 <scott5443003@gmail.com>
2024-10-13 20:12:15 +1100

    Fix some bugs | Xianzhe | Sun Oct 13 20:12:15 AEDT 2024 | xianzhes-MacBook-Pro-9.local

---
+++ 88.merge-sorted-array.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=88 lang=javascript
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  nums1.splice(m, n);
+  nums1.concat(nums2);
+  console.log(nums1);
+  nums1.sort((a, b) => a - b);
+};
+// @lc code=end
