ba909a6c523dd6ea0a3de105204091124750c682
Xianzhezhang97 <scott5443003@gmail.com>
2024-10-14 01:02:19 +1100

    Fix some bugs | Xianzhe | Mon Oct 14 01:02:19 AEDT 2024 | Mac

--- 88.merge-sorted-array.js
+++ 88.merge-sorted-array.js
@@ -12,10 +12,12 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
+
 var merge = function (nums1, m, nums2, n) {
   nums1.splice(m, n);
   nums1.push(...nums2);
   nums1.sort((a, b) => a - b);
   console.log(nums1 + ' ' + nums2);
 };
+
 // @lc code=end
